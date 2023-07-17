67df84075eef8499e052a9aa73999d6d728e095d
mario907 <136901578+mario907@users.noreply.github.com>
2023-07-17 14:56:35 +0200

    Add files via upload

---
+++ pages/Skala dyskretna.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+map_parafie = pd.read_excel(r'Dane\clear.xlsx')
+
+customdata = np.dstack((map_parafie['Parafia'], map_parafie['SDDR']))
+
+fig = px.scatter_mapbox(
+    map_parafie[map_parafie['year'] > 1810],
+    lat="lat",
+    lon="lon",
+    hover_name="Parafia",
+    color = 'cat_SDDR',
+    size = 'size',
+    hover_data={'SDDR': True, 'lat': False, 'year': False, 'lon': False, 'size': False},
+    #color_discrete_sequence=["#ee202a", "#f48929","#f7e829","#ffffff","#2fbebb", "#006eb8","#2d3795"],
+    color_discrete_map=map_col,
+    category_orders={"cat_SDDR": ["Extreme-", "Anomalous-", "Very-", "Normal", "Very+", "Anomalous+", "Extreme+"]},
+    zoom=8,
+    height=800,
+    animation_frame="year",
+    animation_group = 'Parafia',
+    title="year"
+)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+fig.show()
